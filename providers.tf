250cf375b248e7f1eb63df97cddc0fbe1ac93783
ps-chattha <55334254+ps-chattha@users.noreply.github.com>
2024-10-10 01:36:40 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,10 +13,4 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  features {}
-}
+
